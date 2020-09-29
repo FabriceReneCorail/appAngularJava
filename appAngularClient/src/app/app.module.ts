76110a10eaaf67a514668aaca38112f57e3a4933
FRC <fabrice.rene-corail@hotmail.fr>
2020-09-29 14:55:46 +0200

    Revert "fix Angular Architecture Project"
    This reverts commit eeb5599fedb6662e338846925d00514543fce3d0.

--- appAngularClient/src/app/app.module.ts
+++ appAngularClient/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -11,8 +10,7 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    NoopAnimationsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
