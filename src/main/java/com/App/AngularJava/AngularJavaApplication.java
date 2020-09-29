ed9913b88f90d1b9864509f0641caa6738c65d3b
FRC <fabrice.rene-corail@hotmail.fr>
2020-09-29 14:16:37 +0200

    isCool cars method
    implementation of an boolean method , like a filter to put on the screen the coolCars only
    
    if you localhost:8080/allCars.

--- src/main/java/com/App/AngularJava/AngularJavaApplication.java
+++ src/main/java/com/App/AngularJava/AngularJavaApplication.java
@@ -12,8 +12,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @SpringBootApplication
@@ -28,7 +31,7 @@ public class AngularJavaApplication {
 @Bean
 ApplicationRunner init(CarRepository repository){
 	return args -> {
-		Stream.of("Ferrari", "Bugatti","Lamborghini","Aston Martin","Alpine","Porsche", "Pagani").forEach(
+		Stream.of("Ferrari", "Bugatti","Lamborghini","Aston Martin","Alpine","Porsche", "Pagani","Fiat","Tesla","Volvo","Renault").forEach(
 				name ->{repository.save(new Car(name));
 				});
 		for (Car car : repository.findAll()) {
@@ -54,6 +57,24 @@ class Car{
 
 	@NonNull
 	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
 }
 
 @RepositoryRestResource
@@ -62,8 +83,25 @@ interface CarRepository extends JpaRepository<Car, Long> {
 
 @RestController
 class carController {
+	// inject dependency to get access to crud and jpa methods
 	private CarRepository repository;
+//Constructor
+	public carController(CarRepository repository){
+		 this.repository = repository;
+	}
 
-	
+	//get
+	@GetMapping("/allCars")
+	public Collection<Car> beautifulCar(){
+		return repository.findAll().stream().
+				filter(this :: isCool)
+				.collect(Collectors.toList());
+	}
+
+	public boolean isCool(Car car){
+		return !car.getName().equals("Fiat") &&
+		       !car.getName().equals("Volvo")&&
+				!car.getName().equals("Renault");
+	}
 
 }
