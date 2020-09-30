bb8b4ce947b85f9ae3438dffb9bd0b180bc1f7b6
FRC <fabrice.rene-corail@hotmail.fr>
2020-09-30 21:55:27 +0200

    add httpClientModule et listCarComponent

--- appAngularClient/src/app/app.module.ts
+++ appAngularClient/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { ListCarComponent } from './list-car/list-car.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListCarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
