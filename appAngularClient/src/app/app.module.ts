1921cd62ce92906a91b36e0ed2b3666eb04f2057
FRC <fabrice.rene-corail@hotmail.fr>
2020-09-30 23:04:38 +0200

    add Material fixed
    1 i had some issues from BrowserModule ... used ng serve to rebuild project.
    2 had problem with MatModule because this angular version need to be more precise in term of import
    for example you must specify import{MatToolbarModule} from '@angular/material/toolbar' ( with the repo you want to instantiate)

--- appAngularClient/src/app/app.module.ts
+++ appAngularClient/src/app/app.module.ts
@@ -6,6 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { ListCarComponent } from './list-car/list-car.component';
+import { MatButtonModule} from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
+
 
 @NgModule({
   declarations: [
@@ -17,6 +25,11 @@ import { ListCarComponent } from './list-car/list-car.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    MatToolbarModule
 
   ],
   providers: [],
